81b260b75d6de0b53bc3111d203afaa76436f81b
Pavan-git22 <pavangowdasr2005@gmail.com>
2026-06-08 13:06:38 +0530

    Delete functions/numpy_func.py/matplotlib.py directory

--- functions/numpy_func.py/matplotlib.py/begin.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-x = ['mon', 'tue', 'wed', 'thu', 'fri']
-y = [10, 20, 30, 40, 50]
-plt.plot(x, y)
-plt.title("sales per day")
-plt.xlabel("day")
-plt.ylabel("sales")
-plt.show()
--- functions/numpy_func.py/matplotlib.py/hist.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-scores = [85, 90, 78, 92, 88 , 95, 80, 91, 87, 89]
-plt.hist(scores, bins=5, edgecolor='black')
-plt.title("Distribution of Scores")
-plt.xlabel("Score Range")
-plt.ylabel("Frequency")
-plt.show()
